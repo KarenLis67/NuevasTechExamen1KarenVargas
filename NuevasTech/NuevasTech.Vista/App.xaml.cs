5642cb70a9e892f0af7c0d30009107106453800c
Karen Vargas Hoyos <kalisbvar.541@gmail.com>
2024-03-20 20:19:45 -0400

    Creacion de estructura de carpetas

---
+++ NuevasTech/NuevasTech.Vista/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NuevasTech.Vista
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ NuevasTech/NuevasTech.Vista/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NuevasTech.Vista
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
